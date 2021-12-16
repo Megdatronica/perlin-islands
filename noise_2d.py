1a7a5869deb769688f0344efe8f4901856cc4ea1
John Woolley <john.woolley@realitymine.com>
2021-12-16 09:33:03 +0000

    Refactor slightly into function

--- noise_2d.py
+++ noise_2d.py
@@ -15,17 +15,28 @@ from PIL import Image
 X_MAX = 20
 Y_MAX = 20
 PIXELS_PER_SQUARE = 10
-
-CANVAS_SIZE = ((X_MAX-1)*PIXELS_PER_SQUARE, (Y_MAX-1)*PIXELS_PER_SQUARE)
 BLACK = (0,0,0)
-
 
 def main():
     # random.seed(2)
+    CANVAS_SIZE = ((X_MAX-1)*PIXELS_PER_SQUARE, (Y_MAX-1)*PIXELS_PER_SQUARE)
+    noise = perlin_noise(X_MAX, Y_MAX, PIXELS_PER_SQUARE)
+
+    img = Image.new("RGB", CANVAS_SIZE)
+    for i, j in itertools.product(range(CANVAS_SIZE[0]), range(CANVAS_SIZE[1])):
+        value = noise[i, j]
+        # The values seem to range from -0.7 to +0.7 at the extremes, so normalise within this range
+        brightness = 128 + round((value / 0.7) * 128)
+        img.putpixel((i, j), (brightness, brightness, brightness))
+
+    img.save("noise.png")
+    subprocess.run(["open", "noise.png"])
+
+def perlin_noise(x_max, y_max, points_per_square):
     random.seed(time.time())
 
     const_vectors = {}
-    for i, j in itertools.product(range(X_MAX), range(Y_MAX)):
+    for i, j in itertools.product(range(x_max), range(y_max)):
         x = rand_float(-1.0, 1.0)
         y = rand_float(-1.0, 1.0)
         gradient_vec = normalise((x, y))
@@ -33,47 +44,31 @@ def main():
 
     # print(const_vectors)
 
-    img = Image.new("RGB", CANVAS_SIZE)
-
-    for i, j in itertools.product(range(X_MAX - 1), range(Y_MAX - 1)):
+    result = {}
+    for i, j in itertools.product(range(x_max - 1), range(y_max - 1)):
         # print("square {}".format(i))
-        for a, b in itertools.product(range(PIXELS_PER_SQUARE), range(PIXELS_PER_SQUARE)):
-            displacement_top_left = (-a/PIXELS_PER_SQUARE, -b/PIXELS_PER_SQUARE)
+        for a, b in itertools.product(range(points_per_square), range(points_per_square)):
+            displacement_top_left = (-a/points_per_square, -b/points_per_square)
             offset_dot_product_top_left = dot(const_vectors[i, j], displacement_top_left)
 
-            displacement_top_right = ((PIXELS_PER_SQUARE - a)/PIXELS_PER_SQUARE, -b/PIXELS_PER_SQUARE)
+            displacement_top_right = ((points_per_square - a)/points_per_square, -b/points_per_square)
             offset_dot_product_top_right = dot(const_vectors[i+1, j], displacement_top_right)
 
-            displacement_bottom_left = (-a/PIXELS_PER_SQUARE, (PIXELS_PER_SQUARE - b)/PIXELS_PER_SQUARE)
+            displacement_bottom_left = (-a/points_per_square, (points_per_square - b)/points_per_square)
             offset_dot_product_bottom_left = dot(const_vectors[i, j+1], displacement_bottom_left)
 
-            displacement_bottom_right = ((PIXELS_PER_SQUARE - a)/PIXELS_PER_SQUARE, (PIXELS_PER_SQUARE - b)/PIXELS_PER_SQUARE)
+            displacement_bottom_right = ((points_per_square - a)/points_per_square, (points_per_square - b)/points_per_square)
             offset_dot_product_bottom_right = dot(const_vectors[i+1, j+1], displacement_bottom_right)
 
-            interpleft = fade_lerp(b/PIXELS_PER_SQUARE, offset_dot_product_top_left, offset_dot_product_bottom_left)
-            interpright = fade_lerp(b/PIXELS_PER_SQUARE, offset_dot_product_top_right, offset_dot_product_bottom_right)
-            interp = fade_lerp(a/PIXELS_PER_SQUARE, interpleft, interpright)
+            interpleft = fade_lerp(b/points_per_square, offset_dot_product_top_left, offset_dot_product_bottom_left)
+            interpright = fade_lerp(b/points_per_square, offset_dot_product_top_right, offset_dot_product_bottom_right)
+            interp = fade_lerp(a/points_per_square, interpleft, interpright)
 
-            x_val = PIXELS_PER_SQUARE*i + a
-            y_val = PIXELS_PER_SQUARE*j + b
-            color = 128 + round(interp * 256)
-            # print(i, j, a, b, interp, color)
+            x_val = points_per_square*i + a
+            y_val = points_per_square*j + b
+            result[x_val, y_val] = interp
 
-            # if ((i, j, a, b) == (0, 1, 4, 2)):
-            #     print("Top left:", const_vectors[i,j], displacement_top_left)
-            #     print("Top right:", const_vectors[i+1,j], displacement_top_right)
-            #     print("Bottom left:", const_vectors[i,j+1], displacement_bottom_left)
-            #     print("Bottom right:", const_vectors[i+1,j+1], displacement_bottom_right)
-            # if ((i, j, a, b) == (1, 1, 0, 2)):
-            #     print("Top left:", const_vectors[i,j], displacement_top_left)
-            #     print("Top right:", const_vectors[i+1,j], displacement_top_right)
-            #     print("Bottom left:", const_vectors[i,j+1], displacement_bottom_left)
-            #     print("Bottom right:", const_vectors[i+1,j+1], displacement_bottom_right)
-            # print(x_val, y_val)
-            img.putpixel((x_val, y_val), (color, color, color))
-
-    img.save("noise.png")
-    subprocess.run(["open", "noise.png"])
+    return result
 
 def rand_float(start=0.0, end=1.0):
     """Return random float in range [start, end)."""
