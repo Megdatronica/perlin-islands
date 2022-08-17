7257ed03d9c189d7abc1d63c3346ed3f603b6fac
John Woolley <john.woolley@realitymine.com>
2022-08-17 13:50:16 +0100

    Curve now in middle of screen

--- curve.py
+++ curve.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import itertools
+from functools import partial
 import math
 import random
 import subprocess
@@ -23,19 +24,9 @@ def main():
     print("Seed: {}".format(seed))
     random.seed(seed)
 
+    fun = generate_curve(WIDTH, HEIGHT)
+
     img = Image.new("RGB", (WIDTH, HEIGHT))
-    coefficient = random.choice([1.0, -1.0])*random.uniform(math.sqrt(HEIGHT)/2, math.sqrt(HEIGHT)*2) / (WIDTH*100)
-    y_offset = random.randint(HEIGHT//4, round(3*HEIGHT/4))
-    # x_offset = random.randint(WIDTH//4, round(3*WIDTH/4))
-    x_offset = 0
-    print("{}*(x + {}) + {}".format(coefficient, x_offset, y_offset))
-
-    while coefficient*(x_offset + WIDTH)*(x_offset + WIDTH) + y_offset <= 0 or coefficient*(x_offset + WIDTH)*(x_offset + WIDTH) + y_offset > HEIGHT:
-        x_offset -= 1
-
-    def fun(x):
-        return coefficient*(x_offset + x)*(x_offset + x) + y_offset
-
     for i in range(WIDTH):
         y = round(fun(i))
         y = min(y, HEIGHT - 1)
@@ -43,6 +34,28 @@ def main():
         img.putpixel((i, y), (255, 0, 0))
     img.save("parabola.png")
     subprocess.run(["open", "parabola.png"])
+
+def generate_curve(width, height):
+    """Return a function x -> y that will produce a nice curve."""
+    coefficient = random.choice([1.0, -1.0])*random.uniform(math.sqrt(height)/2, math.sqrt(height)*2) / (width*100)
+    y_offset = random.randint(height//4, round(3*height/4))
+    # x_offset = random.randint(width//4, round(3*width/4))
+    x_offset = 0
+    print("{}*(x + {}) + {}".format(coefficient, x_offset, y_offset))
+
+    def fun(coefficient, x_offset, y_offset, x):
+        return coefficient*(x_offset + x)*(x_offset + x) + y_offset
+
+    while fun(coefficient, x_offset, y_offset, width) < 0 or fun(coefficient, x_offset, y_offset, width) >= height:
+        x_offset -= 1
+
+    # Put the function roughly in the centre of the image in terms of its height
+    output_values = [fun(coefficient, x_offset, y_offset, x) for x in range(WIDTH)]
+    output_range = max(output_values) - min(output_values)
+    height_adjustment = (HEIGHT//2 - output_range//2) - min(output_values)
+    y_offset += height_adjustment
+
+    return partial(fun, coefficient, x_offset, y_offset)
 
 if __name__ == "__main__":
     main()
